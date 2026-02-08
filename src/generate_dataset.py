6167915b27fce01c5f4eb6b93d84c9d38fcb9964
Kavana Kiran <kavana.ganesh22@gmail.com>
2026-02-08 08:18:16 -0800

    Added verbose explanation for wavelength, temp and power

--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -7,9 +7,9 @@ def generate_dataset(n_samples=5000, path= "/Users/kavanakiran/Documents/AI_Proj
     records = []
 
     for i in tqdm(range(n_samples), desc='simulating data for optical channel'):
-        wavelength = np.random.uniform(1525, 1575)  #wavelngth in nm
-        launch_power = np.random.uniform(-10,10)    #power in dBm
-        temperature = np.random.uniform(20,60)      #temp in celsius
+        wavelength = np.random.uniform(1530, 1570)  #wavelngth in nm - narrow more realistic
+        launch_power = np.random.uniform(-15,5)    #power in dBm - more negative - means harder
+        temperature = np.random.uniform(15,70)      #temp in celsius - wide temp range
         mod_format = np.random.choice(["NRZ", "PAM4", "QAM16"]) # type: ignore
 
 
